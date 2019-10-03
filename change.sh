31b6254bfde1c8fc3b47f014682cc35cd2885473
Ricardo Castaneda <awsric@amazon.com>
2019-10-03 13:49:39 -0500

    Change

--- change.sh
+++ change.sh
@@ -1,1 +1,5 @@
-echo "<h1> $(date) </h1>" >> ./public_html/index.html
+DATE = $(date)
+echo "<h1> ${DATE} </h1>" >> ./public_html/index.html
+git add .
+git commit -m "Change ${DATE}"
+git push origin master
